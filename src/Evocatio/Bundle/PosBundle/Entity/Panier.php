8e0a0d2fa1d3c03d0bfaa946c4b84f10ca92d797
Laurent Breleur <lbreleur@evocatio.com>
2012-10-10 15:43:54 -0400

    Adjustement panier
    Modification of Address entity + add LigneItem entity + Panier

--- src/Evocatio/Bundle/PosBundle/Entity/Panier.php
+++ src/Evocatio/Bundle/PosBundle/Entity/Panier.php
@@ -4,10 +4,44 @@ namespace Evocatio\Bundle\PosBundle\Entity;
 
 class Panier {
 
-    private $items;
+    private $lignes_item;
+    private $total;
+    private $devise;
 
-    public function __construct() {
-        $this->items = new \Doctrine\Common\Collections\ArrayCollection();
+    public function getLinesItem() {
+        return $this->lignes_item;
+    }
+
+    public function setLinesItem($lignes_item) {
+        return $this->lignes_item = $lignes_item;
+    }
+
+    public function addLigneItem(LigneItem $ligne_item) {
+        $this->lignes_item[] = $ligne_item;
+    }
+
+    public function getQuantity() {
+        return $this->quantity;
+    }
+
+    public function setQuantity($quantity) {
+        $this->quantity = $quantity;
+    }
+
+    public function getTotal() {
+        return $this->total;
+    }
+
+    public function setTotal($total) {
+        $this->total = $total;
+    }
+    
+    public function getDevise(){
+        return $this->devise;
+    }
+    
+    public function setDevise($devise){
+        $this->devise = $devise;
     }
 
 }
