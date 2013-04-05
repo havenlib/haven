9fc0f7618038a248ea79880915cd5b3c249862ed
Stéphan Champagne <sc@evocatio.com>
2013-04-05 17:22:37 -0400

    mis symfony 2.2

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit::getLoader();
+return ComposerAutoloaderInitb4b8e31a5aebd45f1d58443bcfa52dac::getLoader();
