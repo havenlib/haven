62b455ec1b5a8b6c4cfb968bb7a9cb6ab480333a
Laurent Breleur <lbreleur@evocatio.com>
2013-04-05 17:17:27 -0400

    Creation des FAQ
    Mise en place des handlers pour form et lecture.

--- src/Evocatio/Bundle/WebBundle/Lib/FaqFormHandler.php
+++ src/Evocatio/Bundle/WebBundle/Lib/FaqFormHandler.php
@@ -2,9 +2,35 @@
 
 namespace Evocatio\Bundle\WebBundle\Lib;
 
+use Evocatio\Bundle\WebBundle\Lib\FaqReadHandler;
+use Symfony\Component\Security\Core\SecurityContext;
+use Symfony\Component\Form\FormFactory;
 
 class FaqFormHandler {
-    //put your code here
+
+    protected $read_handler; // devrait être son listhandler je pense
+    protected $form_factory;
+    protected $security_context;
+
+    public function __construct(FaqReadHandler $read_handler, SecurityContext $security_context, FormFactory $form_factory) {
+        $this->read_handler = $read_handler;
+        $this->form_factory = $form_factory;
+        $this->security_context = $security_context;
+    }
+
+    /**
+     *  Create the simple delete form
+     * @param integer $id
+     * should create an abstract form handler that whould have that one already
+     * @return form
+     */
+    public function createDeleteForm($id) {
+        return $this->form_factory->createBuilder('form', array('id' => $id))
+                        ->add('id', 'hidden')
+                        ->getForm()
+        ;
+    }
+
 }
 
 ?>
