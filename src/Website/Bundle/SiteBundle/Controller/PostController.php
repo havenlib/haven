03d13e46d40fea229fd25148d2db742010d5bbb3
Laurent Breleur <laurent.breleur@gmail.com>
2013-04-06 21:32:00 -0400

    Quick Fix: Fasq Controller

--- src/Website/Bundle/SiteBundle/Controller/PostController.php
+++ src/Website/Bundle/SiteBundle/Controller/PostController.php
@@ -3,7 +3,6 @@
 namespace Website\Bundle\SiteBundle\Controller;
 
 // Sensio includes
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
