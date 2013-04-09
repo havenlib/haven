6c32a451aeb3bb91472366e0f87e3d214abe37b8
Laurent Breleur <lbreleur@evocatio.com>
2013-04-09 09:36:53 -0400

    Nettoyage
    Supprimer ancien SitewebSiteBundle + Correction dans le routing.

--- src/XWebsite/Bundle/SiteBundle/Controller/FaqController.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Website\Bundle\SiteBundle\Controller;
-use Evocatio\Bundle\WebBundle\Controller\FaqController as BaseFaqController;
-
-class FaqController extends BaseFaqController {
-}
