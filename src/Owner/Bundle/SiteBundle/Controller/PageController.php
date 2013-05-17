e34a05345e0989dbbeeee806168b02fe3a590f81
Laurent Breleur <laurent.breleur@gmail.com>
2013-05-16 20:48:39 -0400

    IMplementation du OwnerBUndle
    Mise en place du menu, routes, templates, controller etc... pour le owner bundle.

---
+++ src/Owner/Bundle/SiteBundle/Controller/PageController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Owner\Bundle\SiteBundle\Controller;
+
+use Evocatio\Bundle\CmsBundle\Controller\PageController as BasePageController;
+
+class PageController extends BasePageController {
+    
+}
+
+?>
