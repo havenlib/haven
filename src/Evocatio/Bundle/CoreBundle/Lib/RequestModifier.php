6c0aa7df9bb9017ca814e931cb392c920a8734d9
Laurent Breleur <laurent.breleur@gmail.com>
2013-06-13 13:30:39 -0400

    Request Modifier + File
    Mise en place du request modifier avec la gestion des fichiers. Il a fallu ramener de prestige
    les informations à propos des fichiers et de l'uploads.
    S'assurer que lors d'un upload multiple il y a des [] devant le nom du champs.

--- src/Evocatio/Bundle/CoreBundle/Lib/RequestModifier.php
+++ src/Evocatio/Bundle/CoreBundle/Lib/RequestModifier.php
@@ -6,17 +6,30 @@ class RequestModifier {
 
     private $request;
     private $slugifier;
+    private $uploader;
 
-    function __construct(Slugifier $slugifier) {
+    function __construct(Slugifier $slugifier, Uploader $uploader) {
         $this->slugifier = $slugifier;
+        $this->uploader = $uploader;
     }
 
     function slug($fields) {
 
         if (empty($this->request))
-            throw new \Exception("You should pass request through setRequest method");
+            throw new \Exception(get_class($this) . " expected a \Symfony\Component\HttpFoundation\Request object. You must pass it through setRequest.");
 
-        $result = $this->slugifier->slugifyRequest($this->request->request->all(), $fields);
+        $result = $this->slugifier->slugifyRequest($this->request, $fields);
+        $this->request->request->replace($result);
+
+        return $this;
+    }
+
+    public function upload() {
+
+        if (empty($this->request))
+            throw new \Exception(get_class($this) . " expected a \Symfony\Component\HttpFoundation\Request object. You must pass it through setRequest.");
+
+        $result = $this->uploader->moveFilesToTempAndMerge($this->request->files->all(), $this->request->request->all());
         $this->request->request->replace($result);
 
         return $this;
