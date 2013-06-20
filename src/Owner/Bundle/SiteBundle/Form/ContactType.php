5c85aabc60e765bb5e42009d6f81545c4367479b
Laurent Breleur <lbreleur@evocatio.com>
2013-06-20 16:47:48 -0400

    Contact
    Construction du formulaire de contact.

--- src/Owner/Bundle/SiteBundle/Form/ContactType.php
+++ src/Owner/Bundle/SiteBundle/Form/ContactType.php
@@ -4,8 +4,12 @@ namespace Owner\Bundle\SiteBundle\Form;
 use Symfony\Component\Form\AbstractType;
 
 class ContactType extends AbstractType{
+    
+    public function buildForm(\Symfony\Component\Form\FormBuilderInterface $builder, array $options) {
+    }
+    
     public function getName() {
-        
+     return "owner_bundle_sitebundle_contacttype";   
     }
 }
 
