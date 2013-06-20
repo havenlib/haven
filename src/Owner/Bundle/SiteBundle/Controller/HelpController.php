b6c258e7966e3fc7e8f5286b990ca0cc959455f0
Laurent Breleur <lbreleur@evocatio.com>
2013-06-20 16:40:14 -0400

    Début formulaire contact
    Ajout du formulaire + Controller.

---
+++ src/Owner/Bundle/SiteBundle/Controller/HelpController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Owner\Bundle\SiteBundle\Controller;
+
+use Symfony\Component\DependencyInjection\ContainerAware;
+
+class HelpController extends ContainerAware {
+
+    public function createContactAction() {
+        
+    }
+
+}
+
+?>
---
+++ src/Owner/Bundle/SiteBundle/Form/ContactType.php
@@ -0,0 +1,12 @@
+<?php
+namespace Owner\Bundle\SiteBundle\Form;
+
+use Symfony\Component\Form\AbstractType;
+
+class ContactType extends AbstractType{
+    public function getName() {
+        
+    }
+}
+
+?>
