e31353eb8dafcb0960f478d732915179f40bc0a4
Laurent Breleur <lbreleur@evocatio.com>
2013-06-25 14:55:34 -0400

    Users
    Ajustement des routes pour le CRUD utilisateur.
    Ajustement des Handlers pour utiliser la nouvelle architecture et qu'ils soient dans les bosn répertoires.
    Ajustement de al configuration.

--- src/Owner/Bundle/SiteBundle/Controller/UserController.php
+++ src/Owner/Bundle/SiteBundle/Controller/UserController.php
@@ -5,5 +5,8 @@ namespace Owner\Bundle\SiteBundle\Controller;
 use Evocatio\Bundle\SecurityBundle\Controller\UserController as BaseUserController;
 
 class UserController extends BaseUserController {
-    
+
+    protected function redirectListAction() {
+        return $this->redirectAction('owner_site_user_list', array(), array('user', 'list'));
+    }
 }
