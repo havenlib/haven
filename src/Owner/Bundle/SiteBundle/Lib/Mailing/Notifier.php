3ee85e7a684265f2fb6f6562c6eb3934612ad370
Laurent Breleur <lbreleur@evocatio.com>
2013-06-26 10:01:41 -0400

    Mail d'activation
    Mise en place du système d'activation avec la gestion de la route d'activation.
    Tests d'envoi et de réception du mail.

--- src/Owner/Bundle/SiteBundle/Lib/Mailing/Notifier.php
+++ src/Owner/Bundle/SiteBundle/Lib/Mailing/Notifier.php
@@ -33,19 +33,19 @@ class Notifier extends BaseNotifier {
         $this->addToPool($mail);
     }
 
-    public function createNewUserNotification($reset) {
+    public function createNewUserNotification($reset, $reset_url) {
         $mail = new MailMessage();
         $mail->setTo(array($reset->getUser()->getEmail()));
         $mail->setFrom(array($this->notification['from_adresses']['default']['email']), $this->notification['from_adresses']['default']['name']);
 
         $data['%confirmation_code%'] = $reset->getConfirmation();
-        $data['%confirmation_url%'] = $reset->getConfirmation();
+        $data['%confirmation_url%'] = $reset_url;
 
         $subject = $this->translator->trans('subject.new_user_account', array(), "mails");
         $message = $this->translator->trans('message.new_user_account', $data, "mails");
 
-        $body = $this->templating->render("OwnerSiteBundle:Mail:template/default.html.twig", array(
-            "message" => $message
+        echo $body = $this->templating->render("OwnerSiteBundle:Mail:template/default.html.twig", array(
+    "message" => $message
         ));
 
         $mail->setBody($body, 'text/html');
