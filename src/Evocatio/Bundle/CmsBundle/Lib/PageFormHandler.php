008db4224e12b71f66b1fddeef944f90edb263c3
Laurent Breleur <lbreleur@evocatio.com>
2013-08-05 10:05:00 -0400

    Correction du form
    Correction du formulaire de language.

--- src/Evocatio/Bundle/CmsBundle/Lib/PageFormHandler.php
+++ src/Evocatio/Bundle/CmsBundle/Lib/PageFormHandler.php
@@ -7,27 +7,19 @@ use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\Form\FormFactory;
 use Evocatio\Bundle\CmsBundle\Entity\Page as Entity;
 use Evocatio\Bundle\CmsBundle\Form\PageType as Type;
-use Monolog\Logger;
-use Monolog\Handler\StreamHandler;
 
 class PageFormHandler {
 
-    protected $read_handler; // devrait être son listhandler je pense
+    protected $read_handler;
     protected $language_read_handler;
     protected $form_factory;
     protected $security_context;
-    protected $logger;
 
     public function __construct(PageReadHandler $read_handler, LanguageReadHandler $language_read_handler, SecurityContext $security_context, FormFactory $form_factory) {
         $this->read_handler = $read_handler;
         $this->language_read_handler = $language_read_handler;
         $this->form_factory = $form_factory;
         $this->security_context = $security_context;
-
-        // create a log channel
-        $this->logger = new Logger('general');
-        $this->logger->pushHandler(new StreamHandler('/home/lbreleur/workspace/evocatio/sites2/app/logs/general.log'));
-        $this->logger->addInfo(__CLASS__ . " buildind done");
     }
 
     public function createEditForm($id) {
@@ -38,7 +30,7 @@ class PageFormHandler {
     }
 
     public function createNewForm($data = null) {
-        return $form = $this->doCreate($this->getDefaultTypeClass(), $data);
+        return $form = $this->form_factory->create(new Type(), $data);
     }
 
     /**
