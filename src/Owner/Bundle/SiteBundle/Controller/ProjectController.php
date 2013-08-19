ae126bbda3c0fb7dd498be65ff0cc90ddf8b0657
Laurent Breleur <lbreleur@evocatio.com>
2013-08-19 13:40:56 -0400

    Ajustement des vues FAQ
    Ajustement des vues FAQ pour le siteweb.

---
+++ src/Owner/Bundle/SiteBundle/Controller/ProjectController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Owner\Bundle\SiteBundle\Controller;
+
+use Evocatio\Bundle\PortfolioBundle\Controller\ProjectController as BaseController;
+
+class ProjectController extends BaseController {
+
+}
+
+?>
